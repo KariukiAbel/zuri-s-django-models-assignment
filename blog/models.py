fa793dcb2ea8abc9f63d78b7fb48d674ad58ae14
KariukiAbel <abelmuriithi@gmail.com>
2022-06-18 09:35:10 +0300

    Django models intro

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+
+# Create your models here.
+class BlogModel(models.Model):
+    Title= models.CharField(max_length=200)
+    Text = models.TextField(max_length=500)
+    Author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    Created_Date = models.DateTimeField(auto_now_add=True)
+    Published_date = models.DateTimeField(auto_now=False )
+    
+    def __str__(self):
+        return self.Title
