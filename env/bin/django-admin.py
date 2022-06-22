5437ec7ed60ebb5089bf390e91f415b3077e6dc3
KariukiAbel <abelmuriithi@gmail.com>
2022-06-22 16:53:00 +0300

    Added the environment dependencies

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nabesh/Visual Studio Projects/Zuri/python files/Zuri django models/I4G012951TLJ/env/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
