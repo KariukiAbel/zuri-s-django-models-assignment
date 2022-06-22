1edb76f09047ce2a92b78813492f7a56ea20ca81
KariukiAbel <abelmuriithi@gmail.com>
2022-06-22 16:11:55 +0300

    Most of the missing files are now added

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from blog.models import Post
 from django.contrib import admin
-from .models import Post
+from .models import Post, BlogModel
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(BlogModel)
+# admin.site.register(Post)
